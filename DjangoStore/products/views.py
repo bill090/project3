3e5fc6912cc7cd343a4c1275e8ed1256127dbfd0
William LIAO <william.bw.liao@gmail.com>
2021-05-06 09:34:58 -0400

    Homepage refactoring

--- DjangoStore/products/views.py
+++ DjangoStore/products/views.py
@@ -7,25 +7,25 @@ from .models import Product, Category
 # Create your views here.
 
 def Home(request):
-    products = Product.objects.all()
-    context = {"all_products_list": list(products), 'listAll': False}
+    products = Product.objects.filter(featured=True)
+    context = {"all_products_list": list(products)}
     return render(request, 'products/home.html', context)
 
 def CategoryView(request, slug):
     category = Category.objects.filter(slug=slug)[0]
     products = Product.objects.filter(category=category)
-    context = {"all_products_list": list(products), 'listAll': True}
+    context = {"all_products_list": list(products)}
     return render(request, 'products/home.html', context)
 
 def AllView(request):
     products = Product.objects.all()
-    context = {"all_products_list": list(products), 'listAll': True}
+    context = {"all_products_list": list(products)}
     return render(request, 'products/home.html', context)
 
 def SearchView(request):
     query = request.GET.get('q')
     products = Product.objects.filter(name__icontains=query)
     
-    context = {"all_products_list": list(products), 'listAll': True}
+    context = {"all_products_list": list(products)}
 
     return render(request, 'products/home.html', context)
